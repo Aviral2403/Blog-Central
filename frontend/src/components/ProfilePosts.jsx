4740d4112422893440d398d89b35dd4ebba04d77
Aviral2403 <140821586+Aviral2403@users.noreply.github.com>
2024-08-03 22:09:59 +0530

    Update Styling for ProfilePosts.jsx for better UI

--- frontend/src/components/ProfilePosts.jsx
+++ frontend/src/components/ProfilePosts.jsx
@@ -3,33 +3,33 @@ import { IF } from "../url";
 /* eslint-disable react/prop-types */
 const ProfilePosts = ({ p, navigateToPost }) => {
   return (
-    <div className="w-full flex flex-col md:flex-row mt-8 space-y-4 md:space-y-0 md:space-x-4 cursor-pointer"
-    onClick={() => navigateToPost(p._id)}>
-      <div className="w-full flex flex-col md:flex-row mt-8 space-y-4 md:space-y-0 md:space-x-4">
-        {/* left */}
-        <div className="w-full md:w-[35%] h-[200px] flex justify-center items-center">
-          <img
-            src={IF + p.photo}
-            alt=""
-            className="h-full w-full object-cover"
-          />
-        </div>
-        {/* right */}
-        <div className="flex flex-col w-full md:w-[65%] space-y-2 md:space-y-0">
-          <h1 className="text-xl font-bold md:mb-2 mb-1 md:text-2xl">
-            {p.title}
-          </h1>
-          <div className="flex flex-col md:flex-row justify-between text-sm font-semibold text-gray-500">
-            <p>@{p.username}</p>
-            <div className="flex space-x-2">
-              <p>{new Date(p.updatedAt).toString().slice(0, 15)}</p>
-              <p>{new Date(p.updatedAt).toString().slice(16, 24)}</p>
-            </div>
+    <div 
+      className="w-full flex flex-col cursor-pointer mt-8 md:flex-col lg:flex-row space-y-4 lg:space-y-0 lg:space-x-4"
+      onClick={() => navigateToPost(p._id)}
+    >
+      {/* left */}
+      <div className="w-full lg:w-[35%] h-[200px] flex justify-center items-center">
+        <img
+          src={IF + p.photo}
+          alt=""
+          className="h-full w-full object-cover"
+        />
+      </div>
+      {/* right */}
+      <div className="flex flex-col w-full lg:w-[65%] space-y-2">
+        <h1 className="text-xl font-bold mb-1 md:text-2xl">
+          {p.title}
+        </h1>
+        <div className="flex flex-col md:flex-row justify-between text-sm font-semibold text-gray-500">
+          <p>@{p.username}</p>
+          <div className="flex space-x-2">
+            <p>{new Date(p.updatedAt).toString().slice(0, 15)}</p>
+            <p>{new Date(p.updatedAt).toString().slice(16, 24)}</p>
           </div>
-          <p className="text-sm md:text-lg">
-          {p.desc.slice(0,200)} <span className="text-blue-500">...Read More</span>
-          </p>
         </div>
+        <p className="text-sm md:text-lg">
+          {p.desc.slice(0, 200)} <span className="text-blue-500">...Read More</span>
+        </p>
       </div>
     </div>
   );
