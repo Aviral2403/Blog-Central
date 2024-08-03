76b36fabf9ededecce02e3dec257f2accf71043e
Aviral2403 <140821586+Aviral2403@users.noreply.github.com>
2024-08-03 12:49:06 +0530

    improved styling for home-posts

--- frontend/src/components/HomePosts.jsx
+++ frontend/src/components/HomePosts.jsx
@@ -1,31 +1,39 @@
 import { IF } from "../url";
 
 /* eslint-disable react/prop-types */
-const HomePosts = ({post}) => {
+const HomePosts = ({ post }) => {
   return (
-    <div className="w-full flex flex-col md:flex-row mt-8 space-y-4 md:space-y-0 md:space-x-4">
-      {/* left */}
-      <div className="w-full md:w-[35%] h-[200px] flex justify-center items-center">
+    <div className=" lg:px-16 w-full flex flex-col mt-8 space-y-4 lg:flex-row lg:space-y-0 lg:space-x-4">
+      {/* Post Title */}
+      <h1 className="text-xl font-bold mb-1 sm:mb-2 sm:text-2xl lg:hidden">
+        {post.title}
+      </h1>
+
+      {/* Post Image */}
+      <div className="w-full h-[40vh] sm:h-[40vh] md:h-[40vh] lg:w-[35%] lg:h-[200px] flex justify-center items-center">
         <img
-          src={IF+post.photo}
+          src={IF + post.photo}
           alt=""
           className="h-full w-full object-cover"
         />
       </div>
-      {/* right */}
-      <div className="flex flex-col w-full md:w-[65%] space-y-2 md:space-y-0">
-        <h1 className="text-xl font-bold md:mb-2 mb-1 md:text-2xl">
+
+      {/* Post Details */}
+      <div className="flex flex-col w-full space-y-2 lg:w-[65%] lg:space-y-0">
+        {/* Show title only for lg screens */}
+        <h1 className="text-xl font-bold mb-1 lg:mb-2 lg:text-2xl hidden lg:block">
           {post.title}
         </h1>
-        <div className="flex flex-col md:flex-row justify-between text-sm font-semibold text-gray-500">
+        <div className="flex flex-col justify-between text-sm font-semibold text-gray-500">
           <p>@{post.username}</p>
           <div className="flex space-x-2">
-            <p>{new Date(post.updatedAt).toString().slice(0,15)}</p>
-            <p>{new Date(post.updatedAt).toString().slice(16,24)}</p>
+            <p>{new Date(post.updatedAt).toString().slice(0, 15)}</p>
+            <p>{new Date(post.updatedAt).toString().slice(16, 24)}</p>
           </div>
         </div>
-        <p className="text-sm md:text-lg ">
-        {post.desc.slice(0,200)} <span className="text-blue-500">...Read More</span>
+        <p className="text-sm sm:text-lg">
+          {post.desc.slice(0, 200)}{" "}
+          <span className="text-blue-500">...Read More</span>
         </p>
       </div>
     </div>
@@ -33,4 +41,3 @@ const HomePosts = ({post}) => {
 };
 
 export default HomePosts;
-
