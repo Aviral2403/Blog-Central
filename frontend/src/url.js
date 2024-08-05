0cebe6056121a963c81368d6fa4b26ca9b5a07ac
Aviral2403 <140821586+Aviral2403@users.noreply.github.com>
2024-08-05 22:13:41 +0530

    Update url.js

--- frontend/src/url.js
+++ frontend/src/url.js
@@ -1,2 +1,2 @@
-export const URL = import.meta.env.VITE_APP_API_URL || "https://blog-central-hub.onrender.com"
-export const IF = import.meta.env.VITE_APP_IMAGE_API_URL || "https://blog-central-hub.onrender.com/images/"
+export const URL = import.meta.env.VITE_APP_API_URL 
+export const IF = import.meta.env.VITE_APP_IMAGE_API_URL 
